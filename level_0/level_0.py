1a40bda62acc7ba1e964d16e037970c1b84849d8
martinmsaavedra <martin@bildung.uy>
2021-01-04 20:26:09 +0000

    task 0

--- level_0/level_0.py
+++ level_0/level_0.py
@@ -3,9 +3,9 @@ import requests
 
 
 url = "http://158.69.76.135/level0.php"
-data_key = {"id":"2089"}
+data_key = {"id":"2089", "holdthedoor":"Submit+Query"}
 
-for i in range(2):
+for i in range(1000):
     try:
         requests.post(url, data = data_key)
         print("success")
